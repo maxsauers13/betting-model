e0b4fd8eb247988293db54764a30c9a8c8c6c8ba
maxsauers <msauers2@illinois.edu>
2020-12-20 16:40:13 -0600

    ML, spread, over functionality

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
 import fileReader
+import montecarlo
+import betting
 
 def main():
-    titans = fileReader.getScoresTeam('games-data.csv', 'Tennessee Titans')
-    print(titans)
-    nfl = fileReader.getTotalsLeague('season-data.csv')
-    print(len(nfl))
+    team1 = 'Chicago Bears'
+    team2 = 'Minnesota Vikings'
+
+    team1percentage, team2percentage, team1scores, team2scores = montecarlo.monteCarlo(team1, team2)
+
+    ml1 = betting.moneyline(team1percentage)
+    ml2 = betting.moneyline(team2percentage)
+    spread = betting.spread(team1scores, team2scores)
+    team1spread = ml1[0] + str(spread)
+    team2spread = ml2[0] + str(spread)
+    over = str(betting.over(team1scores, team2scores))
+
+    print(team1 + ' ' + ml1 + ' ' + team1spread + ' +' + over)
+    print(team2 + ' ' + ml2 + ' ' + team2spread + ' -' + over)
 
 main()
--- montecarlo.py
+++ montecarlo.py
@@ -5,6 +5,7 @@ import random
 from scipy.stats import norm
 
 TEAMS = ['Buffalo Bills', 'Miami Dolphins', 'New England Patriots', 'New York Jets', 'Pittsburgh Steelers', 'Cleveland Browns', 'Baltimore Ravens', 'Cincinnati Bengals', 'Indianapolis Colts', 'Tennessee Titans', 'Houston Texans', 'Jacksonville Jaguars', 'Kansas City Chiefs', 'Las Vegas Raiders', 'Denver Broncos', 'Los Angeles Chargers', 'Washington Football Team', 'New York Giants', 'Philadelphia Eagles', 'Dallas Cowboys', 'Green Bay Packers', 'Minnesota Vikings', 'Chicago Bears', 'Detroit Lions', 'New Orleans Saints', 'Tampa Bay Buccaneers', 'Atlanta Falcons', 'Carolina Panthers', 'Los Angeles Rams', 'Seattle Seahawks', 'Arizona Cardinals', 'San Francisco 49ers']
+MONTE_CARLO_ITERATIONS = 10000
 
 def getAverages(teamName):
     totals = fileReader.getTotalsTeam('season-data.csv', teamName)
@@ -29,19 +30,35 @@ def getStDevPoints(teamName):
 
 def getNorms(team1, team2):
     team1adj, team2adj = getAdjPoints(team1, team2)
-    print(team1adj, team2adj)
 
     std1 = getStDevPoints(team1)
     std2 = getStDevPoints(team2)
-    print(std1, std2)
 
     rand1 = random.random()
     rand2 = random.random()
 
-    norm1 = 1 / norm.cdf(rand1)
-    norm2 = 1 / norm.cdf(rand2)
+    norm1 = norm.ppf(rand1, team1adj, std1)
+    norm2 = norm.ppf(rand2, team2adj, std2)
     
     return norm1, norm2
 
-a, b = getNorms('Jacksonville Jaguars', 'Tennessee Titans')
-print(a, b)
+def monteCarlo(team1, team2):
+    team1wins = 0
+    team1scores = []
+    team2wins = 0
+    team2scores = []
+
+    count = 0
+    while count < MONTE_CARLO_ITERATIONS:
+        team1score, team2score = getNorms(team1, team2)
+
+        if team1score > team2score:
+            team1wins += 1
+        else:
+            team2wins += 1
+        
+        team1scores.append(team1score)
+        team2scores.append(team2score)
+        count += 1
+    
+    return (team1wins / MONTE_CARLO_ITERATIONS) * 100, (team2wins / MONTE_CARLO_ITERATIONS) * 100, team1scores, team2scores
